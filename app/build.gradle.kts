5de351905973d176a4f9058a784316a1636988c8
ArtikciNarek2006 <artikcinarek2006@gmail.com>
2024-01-05 14:04:17 +0400

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.midi_control"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.midi_control"
